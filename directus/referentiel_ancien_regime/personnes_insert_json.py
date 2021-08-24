46234ec402dd834a2c759fbf01cfef3275ab6947
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-08-24 14:22:12 +0200

    update directus personnes_insert_json

--- directus/referentiel_ancien_regime/personnes_insert_json.py
+++ directus/referentiel_ancien_regime/personnes_insert_json.py
@@ -6,7 +6,6 @@ import requests
 import yaml
 from pprint import pprint
 import time
-import random
 
 # Arguments
 parser = argparse.ArgumentParser()
@@ -22,84 +21,136 @@ access_token = r.json()['data']['access_token']
 refresh_token = r.json()['data']['refresh_token']
 file.close()
 
+slice_size = 50
+sleep_time = 2
+
 ########################################################################################
 ## PERSONNES
 ########################################################################################
 
 r = requests.get(secret["url"] + '/items/personnes?limit=-1&access_token=' + access_token)
+
+print("""
+
+COLLECTION 'PERSONNES'
+
+Récupération des données:""")
 print(r)
 ids = [item["id"] for item in r.json()["data"]]
-r = requests.delete(secret["url"] + '/items/personnes?limit=-1&access_token=' + access_token, json=ids)
-print(r)
+
+print("""
+
+""")
+print(len(ids), "données à supprimer:")
+
+for i in range(0, len(ids), slice_size):
+	ids_slice = [ids[j] for j in range(i, i+slice_size) if j < len(ids)]
+	print(i)
+	r = requests.delete(secret["url"] + '/items/personnes?limit=-1&access_token=' + access_token, json=ids_slice)
+	print(r)
+
 r = requests.get(secret["url"] + '/items/personnes?limit=-1&access_token=' + access_token)
 print(r)
-# print(r.json()["data"])
 
 with open(args.json_concepts) as json_file:
 	data_concepts = json.load(json_file)
-	# random.shuffle(sample)
 
-	pprint(len(data_concepts))
+	print("""
+	
+	""")
+	print(len(data_concepts), "données à insérer:")
 
-	for i in range(0,5200, 50):
-		if i == 0:
-			continue
+	for i in range(0, len(data_concepts), slice_size):
+		# print(data_concepts)
+		personnes_slice = [data_concepts[j] for j in range(i, i+slice_size) if j < len(data_concepts)]
 		print(i)
 		try:
-			r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=data_concepts[i-50:i])
+			r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=personnes_slice)
 			r.raise_for_status()
 		except Exception as e:
 			print(e)
 		print(r)
-		time.sleep(2)
+		time.sleep(sleep_time)
 
-	# r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=data_concepts[5200:5241])
-	# print(r)
+	# for i in range(0,5200, slice_size):
+	# 	if i == 0:
+	# 		continue
+	# 	print(i)
+	# 	try:
+	# 		r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=data_concepts[i-slice_size:i])
+	# 		r.raise_for_status()
+	# 	except Exception as e:
+	# 		print(e)
+	# 	print(r)
+	# 	time.sleep(sleep_time)
 
-	for i in data_concepts[5200:5241]:
-		print(i)
-		try:
-			r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=i)
-			r.raise_for_status()
-		except Exception as e:
-			print(e)
-			print(i)
-		print(r)
-		time.sleep(2)
-
-
-# sys.exit()
+	# for i in data_concepts[5200:5241]:
+	# 	print(len(i))
+	# 	try:
+	# 		r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=i)
+	# 		r.raise_for_status()
+	# 	except Exception as e:
+	# 		print(e)
+	# 	print(r)
+	# 	time.sleep(sleep_time)
 
 ########################################################################################
 ## INDEXATIONS
 ########################################################################################
 
+print("""
+
+##########################################################################################
+
+COLLECTION 'SOURCES ARTICLES'
+
+Récupération des données:""")
 r = requests.get(secret["url"] + '/items/sources_articles?limit=-1&access_token=' + access_token)
 print(r)
+
 ids = [item["id"] for item in r.json()["data"]]
-r = requests.delete(secret["url"] + f'/items/sources_articles?limit=-1&access_token=' + access_token, json=ids)
-print(r)
+
+print("""
+
+""")
+print(len(ids), "données à supprimer:""")
+for i in range(0, len(ids), slice_size):
+	ids_slice = [ids[j] for j in range(i, i+slice_size) if j < len(ids)]
+
+	r = requests.delete(secret["url"] + '/items/sources_articles?limit=-1&access_token=' + access_token, json=ids_slice)
+	print(r)
+
 r = requests.get(secret["url"] + '/items/sources_articles?limit=-1&access_token=' + access_token)
 print(r)
-# print(r.json())
 
 with open(args.json_index) as json_file:
 	data_indexation = json.load(json_file)
-	# random.shuffle(sample)
 
-	print(len(data_indexation))
+	print("""
 
-	for i in range(0, len(data_indexation), 50):
-		if i == 0:
-			continue
+	""")
+	print(len(data_indexation), "données à insérer:")
+
+	for i in range(0, len(data_indexation), slice_size):
+		# print(data_concepts)
+		indexation_slice = [data_indexation[j] for j in range(i, i+slice_size) if j < len(data_indexation)]
 		print(i)
-		try:
-			r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=data_indexation[i-50:i])
-			r.raise_for_status()
-		except Exception as e:
-			print(e)
+		r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=indexation_slice)
+		r.raise_for_status()
 		print(r)
-		time.sleep(2)
+		time.sleep(sleep_time)
 
-	r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=data_indexation[1400:1414])
-	print(r)
+	# for i in range(0, len(data_indexation), slice_size):
+	# 	if i == 0:
+	# 		continue
+	# 	print(i)
+	# 	try:
+	# 		r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=data_indexation[i-slice_size:i])
+	# 		r.raise_for_status()
+	# 	except Exception as e:
+	# 		print(e)
+	# 	print(r)
+	# 	time.sleep(sleep_time)
+	#
+	# r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=data_indexation[1400:1414])
+	# print(r)
