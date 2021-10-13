03267cde0fdbcbf5b8d8202ab16cbf374e631471
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-10-13 16:48:57 +0200

    fix directus-euterpe-transfert-images

--- directus/euterpe/transfert-images.py
+++ directus/euterpe/transfert-images.py
@@ -2,7 +2,9 @@ import os
 import sys
 import requests
 import yaml
-from time import time
+import time
+import base64
+import json
 
 # YAML Secret
 file = open(os.path.join(sys.path[0], "secret.yaml"))
@@ -14,29 +16,36 @@ refresh_token = r.json()['data']['refresh_token']
 file.close()
 
 # Checking how many files are currently in Directus
-r = requests.get(secret["url"] + '/files?limit=-1&access_token=' + access_token)
-print(len(r.json()["data"]), "files currently in Directus-Euterpe\n")
+# r = requests.get(secret["url"] + '/files?limit=-1&access_token=' + access_token)
+# print(len(r.json()["data"]), "files currently in Directus-Euterpe\n")
 
 # Transfering the images
-print("Sending new files to Directus\n")
+print("Sending new files to Directus :\n")
 n = 1
 errors = []
 
-for i in range(1128103, 1130969):
+for i in range(1128746, 1130969):
 	try:
 		print("Sending image", i)
-		files = {'media': open(f'../../../../../../d/images/{i}.JPG', 'rb')}
-		response = requests.post(secret["url"] + '/files?limit=-1&access_token=' + access_token, files=files)
-		print(r, "\n")
-		n += 1
-		print(n, "files sent")
-		time.sleep(5)
+		with open(f'../../../../../../d/images/{i}.JPG', 'rb') as img:
+			name_img = os.path.basename(f'../../../../../../d/images/{i}.JPG')
+			files = {'image': (name_img, img,'image/jpeg',
+			                   {'Expires': '0'})}
+
+			with requests.Session() as s:
+				r = s.post(secret["url"] + '/files?limit=-1&access_token=' + access_token, files=files)
+				print(r.status_code)
+				print(n, "files sent \n")
+				n += 1
 	except:
-		print("error : the file might have already been sent or does not exist")
+		print("error : this file will be added to the list of errors")
+		print(r.status_code)
+		print(r.json())
 		errors.append(i)
+	time.sleep(5)
 
 # Checking if the files were added
 r = requests.get(secret["url"] + '/files?limit=-1&access_token=' + access_token)
 print(len(r.json()["data"]), "files currently in Directus-Euterpe\n")
 
-print(errors)
+print("These file might have already been sent or do not exist:\n" + errors)
