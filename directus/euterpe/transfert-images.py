481fff480798a34fde925e0d3f4fa6b1d93c72d2
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-10-14 16:41:08 +0200

    Update transfert-images.py

--- directus/euterpe/transfert-images.py
+++ directus/euterpe/transfert-images.py
@@ -24,32 +24,26 @@ print("Sending new files to Directus :\n")
 n = 1
 errors = []
 
-# Anticipating Directus crash due to request overload
-status_code = 200
+for i in range(1129863, 1130969):
+	try:
+		print("Sending image", i)
 
-while status_code != 401:
+		with open(f'../../../../../../d/images/{i}.JPG', 'rb') as img:
+			name_img = os.path.basename(f'../../../../../../d/images/{i}.JPG')
+			files = {'image': (name_img, img,'image/jpeg',
+			                   {'Expires': '0'})}
 
-	for i in range(1129072, 1130969):
-		try:
-			print("Sending image", i)
-
-			with open(f'../../../../../../d/images/{i}.JPG', 'rb') as img:
-				name_img = os.path.basename(f'../../../../../../d/images/{i}.JPG')
-				files = {'image': (name_img, img,'image/jpeg',
-				                   {'Expires': '0'})}
-
-				with requests.Session() as s:
-					r = s.post(secret["url"] + '/files?limit=-1&access_token=' + access_token, files=files)
-					status_code = r.status_code
-					print(status_code)
-					print("files sent :", n, "\n")
-					n += 1
-		except:
-			print("error : this file will be added to the list of errors")
-			status_code = r.status_code
-			print(status_code)
-			print(r.json(), "\n")
-			errors.append(i)
+			with requests.Session() as s:
+				r = s.post(secret["url"] + '/files?limit=-1&access_token=' + access_token, files=files)
+				print(r.status_code)
+				print("files sent :", n, "\n")
+				n += 1
+	except:
+		print("error : this file will be added to the list of errors")
+		status_code = r.status_code
+		print(status_code)
+		print(r.json(), "\n")
+		errors.append(i)
 
 # Checking if the files were added
 r = requests.get(secret["url"] + '/files?limit=-1&access_token=' + access_token)
