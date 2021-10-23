f24e4c286f12fa78ddbe46b1cb2a13d7256a0e6e
Thomas Bottini <thomas.bottini@gmail.com>
2021-10-23 17:00:45 +0200

    CERES

---
+++ scripts/ceres-kenza-2021.10-make-E13.sh
@@ -0,0 +1,7 @@
+python3 ./rdfizers/ceres/xnview2e13.py \
+    --author_uuid "e4e038d3-1417-4c7a-b2f7-e688615f2f15" \
+    --cache "./caches/ceres/E13.yaml" \
+    --in_xnviewdb "./sources/ceres/2021.10.09_kenza.db" \
+    --out_ttl "./out/ttl/ceres-2021.10.09_kenza.ttl" \
+    --vocabulary_title "vocabulary_title" \
+    --vocabulary_uuid "4d90cb1b-4f16-4c31-8927-5faef7e74998" \
