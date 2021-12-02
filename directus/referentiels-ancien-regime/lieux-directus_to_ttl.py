804be981c125e1882bd7c356bbe628acbff5927f
rbbristow <rebecca.bristow@outlook.fr>
2021-12-02 14:11:55 +0100

    create directus rar lieux_to_ttl

---
+++ directus/referentiels-ancien-regime/lieux-directus_to_ttl.py
@@ -0,0 +1,172 @@
+from rdflib import Graph, Literal, Namespace, DCTERMS, RDF, RDFS, SKOS, URIRef, URIRef as u, Literal as l
+import argparse
+from pprint import pprint
+from sherlockcachemanagement import Cache
+import requests
+import os
+import sys
+import yaml
+import json
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--ttl")
+parser.add_argument("--cache")
+args = parser.parse_args()
+
+# Cache
+cache = Cache(args.cache)
+
+# Secret YAML
+file = open(os.path.join(sys.path[0], "secret.yaml"))
+secret = yaml.full_load(file)
+r = requests.post(secret["url"] + "/auth/login", json={"email": secret["email"], "password": secret["password"]})
+access_token = r.json()['data']['access_token']
+refresh_token = r.json()['data']['refresh_token']
+file.close()
+
+############################################################################################
+## INITIALISATION DU GRAPHE ET NAMESPACES
+############################################################################################
+
+output_graph = Graph()
+
+crm_ns = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+iremus_ns = Namespace("http://data-iremus.huma-num.fr/id/")
+sdt_ns = Namespace("http://data-iremus.huma-num.fr/datatypes/")
+sherlock_ns = Namespace("http://data-iremus.huma-num.fr/ns/sherlock#")
+
+output_graph.bind("crm", crm_ns)
+output_graph.bind("dcterms", DCTERMS)
+output_graph.bind("skos", SKOS)
+output_graph.bind("sdt", sdt_ns)
+output_graph.bind("she_ns", sherlock_ns)
+
+a = RDF.type
+
+def crm(x):
+	return URIRef(crm_ns[x])
+
+def she(x):
+	return URIRef(iremus_ns[x])
+
+def she_ns(x):
+	return URIRef(sherlock_ns[x])
+
+def t(s, p, o):
+	output_graph.add((s, p, o))
+
+
+############################################################################################
+## RECUPERATION DES DONNEES DANS DIRECTUS
+############################################################################################
+
+print("RECUPERATION DES DONNEES DE DIRECTUS")
+
+query = """
+query {
+  lieux(limit: 50) {
+    id
+    label
+    parent {
+      parent_id {
+        label
+      }
+    }
+    periode_historique
+    note_historique
+    alt_label_1
+    alt_label_2
+    etat_actuel
+    fusion {
+      fusion_id {
+        label
+      }
+    }
+    cassini_alignement
+    cassini_voir_aussi
+    geonames_alignement
+    geonames_voir_aussi
+  }
+}"""
+
+r = requests.post(secret["url"] + '/graphql' + '?access_token=' + access_token, json={'query': query})
+print(r.status_code)
+result = json.loads(r.text)
+
+############################################################################################
+## CREATION DES TRIPLETS
+############################################################################################
+
+print("CREATION DU FICHIER TURTLE")
+
+E32_lieux_uri = u(iremus_ns["947a38f0-34ac-4c54-aeb7-69c5f29e77c0"])
+t(E32_lieux_uri, a, crm("E32_Authority_Document"))
+t(E32_lieux_uri, crm("P1_is_identified_by"), l("Lieux"))
+
+for lieu in result["data"]["lieux"]:
+	E93_uuid = lieu["id"]
+	E93_uri = she(E93_uuid)
+	t(E93_uri, a, crm("E93_Presence"))
+	t(E32_lieux_uri, crm("P71_lists"), E93_uri)
+
+	# PrefLabel
+	E41_uri = she(cache.get_uuid(["lieux", E93_uri, "E41"], True))
+	t(E93_uri, crm("P1_is_identified_by"), E41_uri)
+	t(E41_uri, a, crm("E41_Appellation"))
+	t(E41_uri, RDFS.label, l(lieu["label"]))
+	t(E41_uri, crm("P2_has_type"), she("3cf0c743-ee9b-4dfc-8133-7dd383a1b6be"))
+
+	# AltLabels
+	n = 1
+	clé = "alt_label_" + str(n)
+	while clé in lieu.keys():
+		altlabel = lieu[clé]
+		if altlabel != None:
+			E41_alt_uri = she(cache.get_uuid(["lieux", E93_uri, "E41 alt", altlabel], True))
+			t(E41_alt_uri, a, crm("E41_Appellation"))
+			t(E41_alt_uri, RDFS.label, l(altlabel))
+			t(E93_uri, crm("P1_is_identified_by"), E41_alt_uri)
+			t(E41_alt_uri, crm("P2_has_type"), she("70589b95-4156-431e-a58a-818af6dc795a"))
+		n += 1
+		clé = "alt_label_" + str(n)
+
+	# Note historique
+	if lieu["note_historique"] != None:
+		E13_note_uri = she(cache.get_uuid(["lieux", E93_uri, "note historique", "E13"], True))
+		t(E13_note_uri, a, crm("E13_Attribute_Assignement"))
+		t(E13_note_uri, crm("P14_carried_out_by"), she("684b4c1a-be76-474c-810e-0f5984b47921"))
+		t(E13_note_uri, crm("P140_assigned_attribute_to"), E93_uri)
+		t(E13_note_uri, crm("P141_assigned"), l(lieu["note_historique"]))
+		t(E13_note_uri, crm("P177_assigned_property_type"), crm("P3_has_note"))
+
+	# Alignements
+	def alignement(champ, predicat):
+		if lieu[champ] != None:
+			try:
+				url_alignement = lieu[champ].split("'>")[1]
+				url_alignement = url_alignement.replace("</a>", "").replace("</p>", "")
+				t(E93_uri, predicat, u(url_alignement))
+			except:
+				try:
+					url_alignement = lieu[champ].split('">')[1]
+					url_alignement = url_alignement.replace("</a>", "").replace("</p>", "")
+					t(E93_uri, predicat, u(url_alignement))
+				except:
+					print(lieu[champ])
+
+	alignement("cassini_alignement", SKOS.exactMatch)
+	alignement("geonames_alignement", SKOS.exactMatch)
+	alignement("cassini_voir_aussi", SKOS.closeMatch)
+	alignement("geonames_voir_aussi", SKOS.closeMatch)
+	
+
+############################################################################################
+## SERIALISATION DU GRAPHE
+############################################################################################
+
+serialization = output_graph.serialize(format="turtle", base="http://data-iremus.huma-num.fr/id/")
+with open(args.ttl, "wb") as f:
+	f.write(serialization)
+
+cache.bye()
---
+++ scripts/directus-rar-lieux_to_ttl.sh
@@ -0,0 +1,3 @@
+python3 ./directus/referentiels-ancien-regime/lieux-directus_to_ttl.py \
+    --ttl "./out/ttl/rar-lieux.ttl" \
+    --cache "./caches/referentiels-ancien-regime/cache-lieux.yaml"
