f41108e3afdc1ad469deb21ea557c74b1e4f3a22
rbbristow <rebecca.bristow@outlook.fr>
2021-12-03 10:54:23 +0100

    update directus rar lieux to ttl

--- directus/referentiels-ancien-regime/lieux-directus_to_ttl.py
+++ directus/referentiels-ancien-regime/lieux-directus_to_ttl.py
@@ -7,6 +7,9 @@ import os
 import sys
 import yaml
 import json
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
 
 # Arguments
 parser = argparse.ArgumentParser()
@@ -24,6 +27,12 @@ r = requests.post(secret["url"] + "/auth/login", json={"email": secret["email"],
 access_token = r.json()['data']['access_token']
 refresh_token = r.json()['data']['refresh_token']
 file.close()
+
+# Select your transport with a defined url endpoint
+transport = AIOHTTPTransport(url=secret["url"] + '/graphql' + '?access_token=' + access_token)
+
+# Create a GraphQL client using the defined transport
+client = Client(transport=transport, fetch_schema_from_transport=True)
 
 ############################################################################################
 ## INITIALISATION DU GRAPHE ET NAMESPACES
@@ -58,114 +67,124 @@ def t(s, p, o):
 
 
 ############################################################################################
-## RECUPERATION DES DONNEES DANS DIRECTUS
+## CREATION DU VOCABULAIRE CONTROLE
 ############################################################################################
-
-print("RECUPERATION DES DONNEES DE DIRECTUS")
-
-query = """
-query {
-  lieux(limit: 50) {
-    id
-    label
-    parent {
-      parent_id {
-        label
-      }
-    }
-    periode_historique
-    note_historique
-    alt_label_1
-    alt_label_2
-    etat_actuel {
-    	etat_actuel_id {
-        label
-      }
-    }
-    fusion {
-      fusion_id {
-        label
-      }
-    }
-    cassini_alignement
-    cassini_voir_aussi
-    geonames_alignement
-    geonames_voir_aussi
-  }
-}
-
-"""
-
-r = requests.post(secret["url"] + '/graphql' + '?access_token=' + access_token, json={'query': query})
-print(r.status_code)
-result = json.loads(r.text)
-
-############################################################################################
-## CREATION DES TRIPLETS
-############################################################################################
-
-print("CREATION DU FICHIER TURTLE")
 
 E32_lieux_uri = u(iremus_ns["947a38f0-34ac-4c54-aeb7-69c5f29e77c0"])
 t(E32_lieux_uri, a, crm("E32_Authority_Document"))
 t(E32_lieux_uri, crm("P1_is_identified_by"), l("Lieux"))
 
-for lieu in result["data"]["lieux"]:
-	E93_uuid = lieu["id"]
-	E93_uri = she(E93_uuid)
-	t(E93_uri, a, crm("E93_Presence"))
-	t(E32_lieux_uri, crm("P71_lists"), E93_uri)
 
-	# PrefLabel
-	E41_uri = she(cache.get_uuid(["lieux", E93_uri, "E41"], True))
-	t(E93_uri, crm("P1_is_identified_by"), E41_uri)
-	t(E41_uri, a, crm("E41_Appellation"))
-	t(E41_uri, RDFS.label, l(lieu["label"]))
-	t(E41_uri, crm("P2_has_type"), she("3cf0c743-ee9b-4dfc-8133-7dd383a1b6be"))
+############################################################################################
+## RECUPERATION DES DONNEES DANS DIRECTUS
+############################################################################################
 
-	# AltLabels
-	n = 1
-	clé = "alt_label_" + str(n)
-	while clé in lieu.keys():
-		altlabel = lieu[clé]
-		if altlabel != None:
-			E41_alt_uri = she(cache.get_uuid(["lieux", E93_uri, "E41 alt", altlabel], True))
-			t(E41_alt_uri, a, crm("E41_Appellation"))
-			t(E41_alt_uri, RDFS.label, l(altlabel))
-			t(E93_uri, crm("P1_is_identified_by"), E41_alt_uri)
-			t(E41_alt_uri, crm("P2_has_type"), she("70589b95-4156-431e-a58a-818af6dc795a"))
-		n += 1
+print("RECUPERATION DES DONNEES DE DIRECTUS")
+
+query = gql("""
+query ($page_size: Int) {
+	lieux(limit: 500, offset: $page_size) {
+		id
+		label
+		parent {
+		parent_id {
+			label
+		}
+		}
+		periode_historique
+		note_historique
+		alt_label_1
+		alt_label_2
+		etat_actuel {
+			etat_actuel_id {
+			label
+		}
+		}
+		fusion {
+		fusion_id {
+			label
+		}
+		}
+		cassini_alignement
+		cassini_voir_aussi
+		geonames_alignement
+		geonames_voir_aussi
+  }
+}
+""")
+
+page_size = 0
+
+while True:
+
+	response = client.execute(query, variable_values= {"page_size": page_size} )
+
+	#--------------------------------------------------------------------------------------
+	# CREATION DES TRIPLETS
+	#--------------------------------------------------------------------------------------
+	
+	for lieu in response["lieux"]:
+		E93_uuid = lieu["id"]
+		E93_uri = she(E93_uuid)
+		t(E93_uri, a, crm("E93_Presence"))
+		t(E32_lieux_uri, crm("P71_lists"), E93_uri)
+
+		# PrefLabel
+		E41_uri = she(cache.get_uuid(["lieux", E93_uri, "E41"], True))
+		t(E93_uri, crm("P1_is_identified_by"), E41_uri)
+		t(E41_uri, a, crm("E41_Appellation"))
+		t(E41_uri, RDFS.label, l(lieu["label"]))
+		t(E41_uri, crm("P2_has_type"), she("3cf0c743-ee9b-4dfc-8133-7dd383a1b6be"))
+
+		# AltLabels
+		n = 1
 		clé = "alt_label_" + str(n)
+		while clé in lieu.keys():
+			altlabel = lieu[clé]
+			if altlabel != None:
+				E41_alt_uri = she(cache.get_uuid(["lieux", E93_uri, "E41 alt", altlabel], True))
+				t(E41_alt_uri, a, crm("E41_Appellation"))
+				t(E41_alt_uri, RDFS.label, l(altlabel))
+				t(E93_uri, crm("P1_is_identified_by"), E41_alt_uri)
+				t(E41_alt_uri, crm("P2_has_type"), she("70589b95-4156-431e-a58a-818af6dc795a"))
+			n += 1
+			clé = "alt_label_" + str(n)
 
-	# Note historique
-	if lieu["note_historique"] != None:
-		E13_note_uri = she(cache.get_uuid(["lieux", E93_uri, "note historique", "E13"], True))
-		t(E13_note_uri, a, crm("E13_Attribute_Assignement"))
-		t(E13_note_uri, crm("P14_carried_out_by"), she("684b4c1a-be76-474c-810e-0f5984b47921"))
-		t(E13_note_uri, crm("P140_assigned_attribute_to"), E93_uri)
-		t(E13_note_uri, crm("P141_assigned"), l(lieu["note_historique"]))
-		t(E13_note_uri, crm("P177_assigned_property_type"), crm("P3_has_note"))
+		# Note historique
+		if lieu["note_historique"] != None:
+			E13_note_uri = she(cache.get_uuid(["lieux", E93_uri, "note historique", "E13"], True))
+			t(E13_note_uri, a, crm("E13_Attribute_Assignement"))
+			t(E13_note_uri, crm("P14_carried_out_by"), she("684b4c1a-be76-474c-810e-0f5984b47921"))
+			t(E13_note_uri, crm("P140_assigned_attribute_to"), E93_uri)
+			t(E13_note_uri, crm("P141_assigned"), l(lieu["note_historique"]))
+			t(E13_note_uri, crm("P177_assigned_property_type"), crm("P3_has_note"))
 
-	# Alignements
-	def alignement(champ, predicat):
-		if lieu[champ] != None:
-			try:
-				url_alignement = lieu[champ].split("'>")[1]
-				url_alignement = url_alignement.replace("</a>", "").replace("</p>", "")
-				t(E93_uri, predicat, u(url_alignement))
-			except:
+		# Alignements
+		def alignement(champ, predicat):
+			if lieu[champ] != None:
 				try:
-					url_alignement = lieu[champ].split('">')[1]
+					url_alignement = lieu[champ].split("'>")[1]
 					url_alignement = url_alignement.replace("</a>", "").replace("</p>", "")
 					t(E93_uri, predicat, u(url_alignement))
 				except:
-					print(lieu[champ])
+					try:
+						url_alignement = lieu[champ].split('">')[1]
+						url_alignement = url_alignement.replace("</a>", "").replace("</p>", "")
+						t(E93_uri, predicat, u(url_alignement))
+					except:
+						print(lieu[champ])
 
-	alignement("cassini_alignement", SKOS.exactMatch)
-	alignement("geonames_alignement", SKOS.exactMatch)
-	alignement("cassini_voir_aussi", SKOS.closeMatch)
-	alignement("geonames_voir_aussi", SKOS.closeMatch)
-	
+		alignement("cassini_alignement", SKOS.exactMatch)
+		alignement("geonames_alignement", SKOS.exactMatch)
+		alignement("cassini_voir_aussi", SKOS.closeMatch)
+		alignement("geonames_voir_aussi", SKOS.closeMatch)
+
+	print(page_size, "lieux traités")
+	page_size += 500
+
+	if not response["lieux"]:
+		break
+
 
 ############################################################################################
 ## SERIALISATION DU GRAPHE
