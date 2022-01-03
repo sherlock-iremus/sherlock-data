f19f4b79e1f2ac760aff5a9360c9fa5d59b0d7c1
rbbristow <rebecca.bristow@outlook.fr>
2022-01-03 10:21:19 +0100

    rar personnes index

--- scripts/rar-personnes-make-index.sh
+++ scripts/rar-personnes-make-index.sh
@@ -1,6 +1,3 @@
 python3 ./indices/rar-personnes.py \
-  --dburi http://localhost:3030/mercure-galant/sparql \
-  --json ./out/indices/rar-personnes-index.json
-
-# Rebecca : http://localhost:3030/mercure_galant/sparql
-# Thomas : http://localhost:3030/iremus/sparql
+  --ttl "./out/ttl/rar-personnes.ttl" \
+  --json "./out/indices/rar-personnes-index.json"
