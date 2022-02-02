60ba11729e0e73c32fd078616ca9198805d710f4
Thomas Bottini <thomas.bottini@gmail.com>
2022-02-02 16:30:04 +0100

    Scripts

--- scripts/scenario-rar.sh
+++ scripts/scenario-rar.sh
@@ -5,3 +5,6 @@ $(pwd)/scripts/rar-directus-personnes_to_ttl.sh
 
 echo "2. TOPONYMES"
 $(pwd)/scripts/rar-directus-lieux_to_ttl.sh
+
+echo "3. INDEXATION"
+$(pwd)/scripts/rar-directus-indexations_to_ttl.sh
