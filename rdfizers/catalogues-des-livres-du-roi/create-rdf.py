1a53cfdd950ac75236db55da5ac625f757c46487
rbbristow <rebecca.bristow@outlook.fr>
2022-03-21 10:30:26 +0100

    create cat-livres-du-roi create-rdf.py

--- rdfizers/catalogues-des-livres-du-roi/create-rdf.py
+++ rdfizers/catalogues-des-livres-du-roi/create-rdf.py
@@ -35,7 +35,7 @@ for k, v in data["livrets"].items():
     t(F5_uri, a, lrm("F5_Item"))
     t(F5_uri, crm("P1_is_identified_by"), l(k))
     
-    # Evenement de production de l'item
+    # Production de l'item
     F32_uri = she(cache.get_uuid(["livrets", k, "F32", "uuid"], True))
     t(F32_uri, a, lrm("F32_Carrier_Production_Event"))
     t(F32_uri, lrm("R28_produced"), F5_uri)
@@ -46,11 +46,29 @@ for k, v in data["livrets"].items():
     t(F5_uri, lrm("R7_is_materialization_of"), F3_uri)
     t(F32_uri, lrm("R27_materialized"), F3_uri)
 
+    # Creation de la manifestation
+    F30_uri = she(cache.get_uuid(["livrets", k, "F30", "uuid"], True))
+    t(F30_uri, a, lrm("F30_Manifestation_Creation"))
+    t(F30_uri, lrm("R24_created"), F3_uri)
+
     # Expression
     F2_uri = she(cache.get_uuid(["livrets", k, "F2", "uuid"], True))
     t(F2_uri, a, lrm("F2_Expression"))
     t(F3_uri, lrm("R4_embodies"), F2_uri)
 
+    # Creation de l'expression
+    F28_uri = she(cache.get_uuid(["livrets", k, "F28", "uuid"], True))
+    t(F28_uri, a, lrm("F28_Expression_Creation"))
+    t(F28_uri, lrm("R17_created"), F2_uri)
+
+    # Photographie de l'item
+    E65_uri = she(cache.get_uuid(["livrets", k, "E65", "uuid"], True))
+    t(E65_uri, a, crm("E65_Creation"))
+    t(E65_uri, crm("P16_used_specific_object"), F5_uri)
+    t(E65_uri, crm("P14_carried_out_by"), she("710d0c6e-48ed-47b6-b209-00520636d7be"))
+    E36_uri = she(cache.get_uuid(["livrets", k, "E65", "E36", "uuid"], True))
+    t(E36_uri, a, crm("E36_Visual_Item"))
+    t(E65_uri, crm("P94_has_created"), E36_uri)
 
 
 # Sérialisation du graphe
