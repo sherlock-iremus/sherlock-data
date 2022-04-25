8d97de25d1eae9dbf4c2b1f79a41687f77738c5d
Thomas Bottini <thomas.bottini@gmail.com>
2022-04-25 15:41:29 +0200

    fix

--- scripts/mg-mots-clefs_to_ttl.sh
+++ scripts/mg-mots-clefs_to_ttl.sh
@@ -1,4 +1,3 @@
 python3 ./rdfizers/mercure-galant/mg-mots-clefs.py \
   --rdf "./sources/mercure-galant/mots-clefs.rdf" \
-  --ttl "./out/ttl/mots-clefs.ttl" \
-  --cache "./caches/mercure-galant/mots-clefs.yaml"
+  --ttl "./out/ttl/mg-mots-clefs.ttl"
