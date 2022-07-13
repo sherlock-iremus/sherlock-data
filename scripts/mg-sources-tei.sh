6634dc7480254907b1fb626570d7ef9d2cea1234
rbbristow <rebecca.bristow@outlook.fr>
2022-07-13 11:18:28 +0200

    rename

--- scripts/mg-sources-tei.sh
+++ scripts/mg-sources-tei.sh
@@ -1,5 +1,5 @@
 mkdir -p ./caches/mercure-galant/
-python3 ./rdfizers/mercure-galant/main.py \
+python3 ./rdfizers/mercure-galant/mg-sources-tei.py \
     --tei "./mercure-galant/xml" \
     --output_ttl "./out/ttl/mg-sources-tei.ttl" \
     --cache_tei "./caches/mercure-galant/cache-tei.yaml"
