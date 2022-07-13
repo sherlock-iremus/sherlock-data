5f9ae1005a1595b1e36f34ef890e915f8f2a4031
rbbristow <rebecca.bristow@outlook.fr>
2022-07-13 11:16:46 +0200

    rename

--- scripts/mg-indexations-stagiaires.sh
+++ scripts/mg-indexations-stagiaires.sh
@@ -1,6 +1,6 @@
 python3 ./rdfizers/mercure-galant/mg-indexations-stagiaires.py \
   --rdf "./sources/mercure-galant/mots-clefs.rdf" \
 	--txt "./sources/mercure-galant/indexation-stagiaires/" \
-  --ttl "./out/ttl/mg-indexation-stagiaires.ttl" \
+  --ttl "./out/ttl/mg-indexations-stagiaires.ttl" \
   --cache "./caches/mercure-galant/cache-indexation-stagiaires.yaml"\
   --cache_tei "./caches/mercure-galant/cache-tei.yaml"
