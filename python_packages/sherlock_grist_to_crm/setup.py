d202581c7c6cdb25da51ab56274625eb1a8ce408
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2025-09-08 14:55:45 +0200

    🌲

---
+++ python_packages/sherlock_grist_to_crm/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='sherlock_grist_to_crm',
+      packages=['sherlock_grist_to_crm'],
+      version='1.0'
+      )
--- python_packages/sherlock_helpers/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='sherlock_helpers',
-      packages=['sherlock_helpers'],
-      version='1.0'
-      )
