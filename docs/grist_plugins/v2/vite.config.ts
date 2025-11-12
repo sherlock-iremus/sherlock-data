9f0b06b192c5cc8523716281ffa25417ccb89a04
Antoine Lebrun <iremus@MacBook-Pro-de-Antoine.local>
2025-11-12 15:22:46 +0100

    plugin re-written in typescript

--- docs/grist_plugins/v2/vite.config.ts
+++ docs/grist_plugins/v2/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "src",
+    base: '/sherlock-data/grist_plugins/v2/dist/',
     build: {
         outDir: "../dist",
     },
